4a8a84747ef302ec1c834be10e1b60d38c13f28e
hannahannx <4HACKH44@solent.ac.uk>
2021-10-05 18:34:56 +0100

    this is my exercise for the mavenexercises called example1

---
+++ myPracticeCourseWork/session1-copy/maven-test-exercise/example1/src/test/java/org/hannahannx/com504/ood/MyTestClassTest.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.hannahannx.com504.ood;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author hannah-ann
+ */
+public class MyTestClassTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        MyTestClassLog4j myTest = new MyTestClassLog4j();
+        myTest.writeAboutMe();
+    }
+}
